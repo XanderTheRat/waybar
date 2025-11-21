71ba0058186d3fc32d905bbdacade51b56ed2815
XanderTheRat <saussic0canard@gmail.com>
2025-11-21 09:40:43 +0100

    Add battery changement script

--- src/bin/change_battery_mode.rs
+++ src/bin/change_battery_mode.rs
@@ -2,9 +2,9 @@ use std::fs;
 use std::path::Path;
 
 fn main() ->  Result<(), Box<dyn std::error::Error>> {
-	let state_file = "/home/martin/.config/scripts.rs/waybar/battery_state";
+	let state_file = "/home/martin/.config/scripts.rs/waybar_scripts/battery_state";
 	let mut mode = if Path::new(state_file).exists() {
-	        fs::read_to_string(state_file)?.trim().parse::<u8>().unwrap_or(1)
+	        fs::read_to_string(state_file)?.trim().parse::<u8>()?
 	} else {
 		println!("Erreur de nom de fichier");
 		1
