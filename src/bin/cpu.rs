95d9952b086e70061be1d6616018aa28f0aad57b
XanderTheRat <saussic0canard@gmail.com>
2025-11-28 11:45:38 +0100

    Add groups to replace most of the modules in the waybar

---
+++ src/bin/cpu.rs
@@ -0,0 +1,11 @@
+use std::process::Command;
+
+fn main() -> Result<(), Box<dyn std::error::Error>>{
+	let couleur_cpu = "#b794f4";
+
+	let output_cpu = Command::new("sh").arg("-c").arg("awk -v RS=\"\" '{getline; cpu=($2+$4)*100/($2+$4+$5); printf \"%d\", cpu}' /proc/stat").output().expect("Erreur");
+	let cpu = String::from_utf8_lossy(&output_cpu.stdout).trim().to_string();
+
+	println!("<span foreground='{}'>CPU {}%</span>", couleur_cpu, cpu);	
+	Ok(())
+}
--- src/bin/ram.rs
+++ src/bin/ram.rs
@@ -2,11 +2,8 @@ use std::process::{Command, Stdio};
 use std::io::{BufRead, BufReader};
 
 fn main() -> Result<(), Box<dyn std::error::Error>>{
-	let couleur_cpu = "#b794f4";
 	let couleur_mem = "#f687b3";
 
-	let output_cpu = Command::new("sh").arg("-c").arg("awk -v RS=\"\" '{getline; cpu=($2+$4)*100/($2+$4+$5); printf \"%d\", cpu}' /proc/stat").output().expect("Erreur");
-	let cpu = String::from_utf8_lossy(&output_cpu.stdout).trim().to_string();
 	let output_mem = Command::new("free").arg("-m").stdout(Stdio::piped()).spawn().unwrap().stdout.expect("Erreur");
 
 	let reader = BufReader::new(output_mem);
@@ -20,7 +17,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
 				let total: f64 = memory[1].parse()?;
 
 				let percent = (used*100.0/total).round() as u32;
-				println!("<span foreground='{}'>CPU {}%</span> | <span foreground='{}'>MEM {}%</span>", couleur_cpu, cpu, couleur_mem, percent);
+				println!("<span foreground='{}'>MEM {}%</span>", couleur_mem, percent);
 
 			}
 		}
